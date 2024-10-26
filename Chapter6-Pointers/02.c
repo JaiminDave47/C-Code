0df3e8884e2f03b4af4443195766caa7d1041cab
Jaimin Dave <jaimindave477@gmail.com>
2024-10-26 22:59:35 +0530

    Update 02.c

--- Chapter6-Pointers/02.c
+++ Chapter6-Pointers/02.c
@@ -14,8 +14,13 @@ int main(){
 
     // Understanding syntax
 
-    printf("%p",&age); // Prints 'addredd of' variable 'age'
-    printf("%p",ptr1); // Prints 'address of' variable 'age' which is stored in pointer 'ptr'
-    printf("%p",&ptr1); // Prints 'address of' pointer 'ptr'
+    printf("%p\n",&age); // Prints 'address of' variable 'age'
+    printf("%p\n",ptr1); // Prints 'address of' variable 'age' which is stored in pointer 'ptr'
+    printf("%p\n",&ptr1); // Prints 'address of' pointer 'ptr'
+
+    // Each following line will give same output
+    printf("%d\n",age);
+    printf("%d\n",*ptr1);
+    printf("%d\n",*(&age));
     return 0;
 }
